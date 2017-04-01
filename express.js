440dcc7304613c3e648455c7b1f3e3b353725c6f
ricky <huangyunqi@genwoshua.com>
2017-04-01 15:36:58 +0800

    config

--- express.js
+++ express.js
@@ -15,3 +15,22 @@ var port = 7070;
 var server = app.listen(port, function() {
     console.log('Example app listening at http://%s:%s', host, port);
 });
+
+// var proxyRules = new HttpProxyRules({
+//   rules: {
+//     // '/alpha/api/': 'http://42.159.244.26/alpha/api/', // Api Server
+//     '/alpha/api/': 'http://dev.genwoshua.com/alpha/api/', // Api Server
+//
+//     '/alpha/web/': 'http://dev.genwoshua.com/alpha/web/',
+//     '/alpha/static/': 'http://localhost/alpha/static/', // Static on Server
+//     '/tempAuth':'http://router.genwoshua.com/tempAuth',
+//     '/alpha/portal/': 'http://localhost:9001/alpha/portal/',
+//     '/alpha/webapp/': 'http://localhost:9002/alpha/webapp/',
+//     '/': 'http://localhost:7070/',
+//     '/alpha/router/': 'http://localhost:9003/alpha/router/',
+//     '/alpha/qiniu/': 'http://app.genwoshua.com/alpha/qiniu/',
+//
+//
+//   },
+//   default: 'http://dev.genwoshua.com/404.html' // default target
+// });
