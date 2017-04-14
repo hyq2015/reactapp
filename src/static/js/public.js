8a4f5350c14f4e206e356c183597e1cbcea40bc6
ricky <huangyunqi@genwoshua.com>
2017-04-14 18:36:38 +0800

    订单详情

--- src/static/js/public.js
+++ src/static/js/public.js
@@ -1,4 +1,64 @@
+//转换时间函数
+Date.prototype.format = function (format) {
+    var o = {
+        "M+": this.getMonth() + 1,
+        "d+": this.getDate(),
+        "h+": this.getHours(),
+        "m+": this.getMinutes(),
+        "s+": this.getSeconds(),
+        "q+": Math.floor((this.getMonth() + 3) / 3),
+        "S": this.getMilliseconds()
+    };
+    if (/(y+)/.test(format)) {
+        format = format.replace(RegExp.$1, (this.getFullYear() + "").substr(4 - RegExp.$1.length));
+    }
+    for (var k in o) {
+        if (new RegExp("(" + k + ")").test(format)) {
+            format = format.replace(RegExp.$1, RegExp.$1.length == 1 ? o[k] : ("00" + o[k]).substr(("" + o[k]).length));
+        }
+    }
+    return format;
+} ;
+//转换long型为日期字符串
+function getFormatDateByLong(l, pattern) {
+    return getFormatDate(new Date(l), pattern);
+}
+//转换日期对象为日期字符串
+function getFormatDate(date, pattern) {
+    if (date == undefined) {
+        date = new Date();
+    }
+    if (pattern == undefined) {
+        pattern = "yyyy-MM-dd hh:mm:ss";
+    }
+    return date.format(pattern);
+}
+function getSmpFormatDate(date, isFull) {
+    var pattern = "";
+    if (isFull == true || isFull == undefined) {
+        pattern = "yyyy-MM-dd hh:mm:ss";
+    } else if(isFull==2){
+        pattern = "yyyy-MM-dd hh:mm";
+    } else if(isFull==5){
+        pattern = "hh:mm";
+    } else if(isFull==6){
+        pattern = "yyyy-MM";
+    }
+    else {
+        pattern = "yyyy-MM-dd";
+    }
+    return getFormatDate(date, pattern);
+}
 module.exports={
+    /*
+        时间戳转换
+    */
+    getSmpFormatDateByLong:function(date, isFull){
+        if(date=='' || date==null){
+            return
+        }
+        return getSmpFormatDate(new Date(date), isFull);
+    },
     /*
     * 将手机号中间的数字用****代替
     * */
